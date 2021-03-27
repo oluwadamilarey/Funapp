0dd8a97dd1caffeed43e2e2aeb7f1d68e7f00ba8
hardamilare <abimbola301@gmail.com>
2021-03-27 23:39:58 +0100

    fetching brewries json data with httservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ListComponent
+    ListComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
